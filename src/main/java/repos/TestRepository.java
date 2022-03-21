c9e642bf7fcaeb07064e8cd5c90744926f94c0f5
KuryakovArtem <kurik01tagil@gmail.com>
2022-03-21 20:37:37 +0300

    initial commit

--- src/main/java/repos/TestRepository.java
+++ src/main/java/repos/TestRepository.java
@@ -1,4 +1,11 @@
 package repos;
 
-public interface TestRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import project_se.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TestRepository extends JpaRepository<Test, Long> {
+    List<Test> findTestByID(Long id);
 }
